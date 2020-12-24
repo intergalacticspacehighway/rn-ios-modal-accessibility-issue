4efbc1c2556622f9ec89dae6ec3102a30d843de5
Nishan <nishanbende@gmail.com>
2020-12-24 17:07:51 +0530

    inital commit

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Button, View, Modal, Text } from "react-native";
 
 export default function App() {
+  const [showModal, setShowModal] = React.useState(false);
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Button onPress={() => {}} title="This is a dummy button" />
+      <Button onPress={() => setShowModal(true)} title="Open modal" />
+      <Modal visible={showModal} onRequestClose={() => setShowModal(false)}>
+        <View style={{ justifyContent: "center", flex: 1 }}>
+          <Text>
+            Closing this modal should return screenreader focus to the button
+            which opened it
+          </Text>
+          <Button onPress={() => setShowModal(false)} title="Close modal" />
+        </View>
+      </Modal>
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +25,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
